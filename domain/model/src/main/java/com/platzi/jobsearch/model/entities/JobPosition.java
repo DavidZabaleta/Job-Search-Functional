cdc945bd307387ccdf44e1f7ada759e7ec731e3e
David Zabaleta <david.zabaleta@sofka.com.co>
2020-04-01 16:59:36 -0500

    Clean Architecture applied with multi modules
    JobPosition pojo added and the get method for positions

---
+++ domain/model/src/main/java/com/platzi/jobsearch/model/entities/JobPosition.java
@@ -0,0 +1,27 @@
+package com.platzi.jobsearch.model.entities;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JobPosition {
+    private String id;
+    private String type;
+    private String url;
+    @SerializedName(value = "created_at")
+    private String createAt;
+    private String company;
+    @SerializedName(value = "company_url")
+    private String companyUrl;
+    private String title;
+    private String location;
+    private String description;
+    @SerializedName(value = "company_logo")
+    private String companyLogo;
+}
